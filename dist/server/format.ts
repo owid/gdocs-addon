21517419a9586bf5dab1e19f887cf176cc01caa6
Matthieu <matthieu@ourworldindata.org>
2023-09-28 15:05:32 +0000

    refactor: split server files

--- dist/server/format.ts
+++ dist/server/format.ts
@@ -1,31 +1,3 @@
-function main() {
-  format()
-}
-
-function onOpen(e: any) {
-  DocumentApp.getUi()
-    .createAddonMenu()
-    .addItem("Open", "showSidebar")
-    .addItem("Format ArchieML", "format")
-    .addToUi()
-}
-
-function showSidebar() {
-  const html =
-    HtmlService.createHtmlOutputFromFile("sidebar/index").setTitle("OWID Gdocs")
-  DocumentApp.getUi().showSidebar(html)
-}
-
-function getActiveDocument() {
-  const doc = DocumentApp.getActiveDocument()
-  if (doc) return doc
-
-  // If no active document, return a test document (for debugging)
-  return DocumentApp.openByUrl(
-    "https://docs.google.com/document/d/1GkNzLQKD5L-KVzNANoOemztyXRM_3mS5fsyu5bawDOU/edit"
-  )
-}
-
 function format() {
   // Get all paragraphs in the active document
   const paragraphs = getActiveDocument().getBody().getParagraphs()

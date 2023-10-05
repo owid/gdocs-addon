e22c25ac66e0695d1c34ced6f58900de692d1169
Matthieu <matthieu@ourworldindata.org>
2023-10-05 20:22:24 +0000

    feat: add annotate card with support for refs

---
+++ dist/server/annotate.ts
@@ -0,0 +1,34 @@
+function annotateRefs() {
+  const document = getActiveDocument()
+  const selection = document.getSelection()
+
+  // We'll wrap the selected text with {ref} and {/ref} tags. When the selection
+  // is not capturing the whole element (aka. partial selection), we need to get
+  // the respective start and end offsets of the selection to apply the tags
+  // within the element's text, at the edges of the selection. Note that a
+  // selection can span multiple elements.
+  if (selection) {
+    const selectedElements = selection.getRangeElements()
+
+    const firstSelectedElement = selectedElements[0]
+    const lastSelectedElement = selectedElements[selectedElements.length - 1]
+
+    const startOffset = firstSelectedElement.isPartial()
+      ? firstSelectedElement.getStartOffset()
+      : 0
+
+    const endOffset = lastSelectedElement.isPartial()
+      ? lastSelectedElement.getEndOffsetInclusive() + 1
+      : lastSelectedElement.getElement().asText().getText().length
+
+    lastSelectedElement.getElement().asText().insertText(endOffset, "{/ref}")
+    firstSelectedElement.getElement().asText().insertText(startOffset, "{ref}")
+  } else {
+    // When there is no selection, we'll insert the tags at the cursor position.
+    const cursor = document.getCursor()
+    const element = cursor.getElement()
+    const offset = cursor.getSurroundingTextOffset()
+
+    element.asText().insertText(offset, "{ref}{/ref}")
+  }
+}
--- sidebar/index.tsx
+++ sidebar/index.tsx
@@ -1,10 +1,39 @@
 import { render } from "preact"
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Box,
+} from "@mui/material"
 import { FormatCard } from "./Format"
 
 function App() {
   return (
     <div>
       <FormatCard />
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Annotate
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Box display="flex" alignItems="flex-start">
+            <Button
+              variant="contained"
+              onClick={() => google.script.run.annotateRefs()}
+              sx={{ mr: 1, flexShrink: 0 }}
+            >
+              {`{ref}`}
+            </Button>
+            <Typography variant="body2" color="text.secondary">
+              {`Insert or wrap existing text in {ref} tags.`}
+            </Typography>
+          </Box>
+        </CardActions>
+      </Card>
     </div>
   )
 }
